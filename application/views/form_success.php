e0c9da64e3092bfcd883ed75a65523132ce28dbf
jamesa <jamesadcock1980@gmail.com>
2014-03-31 11:35:05 +0100

    added menu

--- application/views/form_success.php
+++ application/views/form_success.php
@@ -34,6 +34,19 @@
             </button>
             <p class="navbar-brand">TeamSync</p>
         </div>
+
+        <!-- Collect the nav links, forms, and other content for toggling -->
+        <div class="collapse navbar-collapse navbar-right navbar-ex1-collapse">
+            <ul class="nav navbar-nav">
+                <li><a href="<?php echo base_url() ?>">Home</a>
+                </li>
+                <li><a href="<?php echo base_url()?>site/contact">Contact</a>
+                </li>
+
+            </ul>
+        </div>
+        <!-- /.navbar-collapse -->
+
     </div>
     <!-- /.container -->
 </nav>
--- application/views/homepage.php
+++ application/views/homepage.php
@@ -29,11 +29,25 @@
                 <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex1-collapse">
                     <span class="sr-only">Toggle navigation</span>
                     <span class="icon-bar"></span>
+
                     <span class="icon-bar"></span>
                     <span class="icon-bar"></span>
                 </button>
                 <p class="navbar-brand">TeamSync</p>
             </div>
+
+            <!-- Collect the nav links, forms, and other content for toggling -->
+            <div class="collapse navbar-collapse navbar-right navbar-ex1-collapse">
+                <ul class="nav navbar-nav">
+                    <li><a href="<?php echo base_url() ?>">Home</a>
+                    </li>
+                    <li><a href="<?php echo base_url()?>site/contact">Contact</a>
+                    </li>
+
+                </ul>
+            </div>
+            <!-- /.navbar-collapse -->
+
         </div>
         <!-- /.container -->
     </nav>
